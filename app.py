270db53d9c40bb1817bda7c0dcc7d3e18b2f2f15
vitpodsokhin <vitpodsokhin@gmail.com>
2023-12-25 01:10:26 +0300

    test run

--- app.py
+++ app.py
@@ -2,12 +2,29 @@
 
 from flask import Flask
 from flask import render_template
+import json, subprocess
 
 app = Flask(__name__)
 
+def get_data():
+    netstat_parser_program = '/Users/v/data/src/github.com/vitpodsokhin/apples/test.py'
+
+    # Run the external program and capture its output
+    result = subprocess.run(['python3', netstat_parser_program], stdout=subprocess.PIPE, text=True)
+    
+    try:
+        process_info = json.loads(json.dumps(result.stdout))
+    except json.JSONDecodeError:
+        process_info = []
+
+    return process_info
+
 @app.route("/")
-def hello() -> str:
-    return "Hello World"
+def index():
+    process_info = get_data()
+
+    return render_template('index.html', process_list=json.dumps(process_info, indent=2))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
