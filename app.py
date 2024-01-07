ba4b98d5584369ffcf2420a38b6a050e2f7d0d43
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-08 01:03:25 +0300

    cosmetic changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask import render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,7 +11,12 @@ from get_data import get_data
 @app.route("/")
 def index():
     processes_with_connections = get_data()
-    return render_template('index.html', process_list=processes_with_connections)
+    return render_template(
+        'index.html',
+        process_list=processes_with_connections,
+        amount_of_processes=len(processes_with_connections),
+        datetime=str(datetime.now()).split('.')[:-1][0]
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
