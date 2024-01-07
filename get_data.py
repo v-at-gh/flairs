5fccb7ebb9fc437e3839acb5096b507a9aa778fd
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-07 19:05:33 +0300

    filter out spooky _listening_ sockets with no local port; add them as a list to a comment for futher investigation of their activity

--- get_data.py
+++ get_data.py
@@ -1,15 +1,47 @@
 #!/usr/bin/env python3
+from typing import List
 
 from src.Netstat import Netstat
 from src.Process import Process
 
-def get_data() -> list:
-    connections = Netstat.get_connections()
+# def get_data() -> List:
+#     connections = Netstat.get_connections()
+#     pids = Netstat.get_pids_of_processes_with_connections(connections)
+#     processes = [Process(pid) for pid in pids]
+#     processes_with_connections = []
+#     for process in processes:
+#         processes_with_connections.append(process.get_connections_of_this_process())
+#     return processes_with_connections
+
+def get_data() -> List:
+    # filter out spooky _listening_ macos processes, such as:
+    #   systemstats
+    #   configd
+    #   locationd
+    #   PerfPowerServices
+    #   bluetoothd
+    #   loginwindow
+    #   airportd
+    #   symptomsd
+    #   wifip2pd
+    #   ControlCenter
+    #   WiFiAgent
+    #   identityservicesd
+    #   sharingd
+    #   WirelessRadioManagerd
+    #   wifivelocityd
+    #   nesessionmanager
+    #   ¯\_(ツ)_/¯
+
+    connections = filter(lambda c: c.localPort != 0, Netstat.get_connections())
+    # connections = Netstat.get_connections()
     pids = Netstat.get_pids_of_processes_with_connections(connections)
     processes = [Process(pid) for pid in pids]
     processes_with_connections = []
     for process in processes:
-        processes_with_connections.append(process.get_connections_of_this_process())
+        processes_with_connections.append(
+                process.get_connections_of_this_process()
+        )
     return processes_with_connections
 
 def main() -> None:
