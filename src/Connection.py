16c7d5c4d49050ba1d7f9b9e9978e4333bd5c957
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-09 00:43:04 +0300

    add doc-strings

--- src/Connection.py
+++ src/Connection.py
@@ -23,6 +23,13 @@ UDP_States = (
 
 class _ConnectionProcessor:
     def __post_init__(self) -> None:
+        '''
+        Process and initialize connection attributes after dataclass initialization.
+
+        This method processes and initializes various connection attributes,
+        such as converting specified attributes to integers, determining the address family,
+        processing socket information, and setting the connection state for UDP connections.
+        '''
         self._convert_to_int('recvQ', 'sendQ', 'pid', 'epid', 'rhiwat', 'shiwat')
         self.family = 4 if self.proto.endswith('4') else 6
         self.proto = ''.join(char for char in self.proto if char.isalpha())
@@ -37,28 +44,48 @@ class _ConnectionProcessor:
                 self.state = 'ESTABLISHED'
 
     def _convert_to_int(self, *attributes) -> None:
+        '''
+        Convert specified attributes to integers.
+
+        Args:
+            *attributes: Variable number of attribute names to be converted to integers.
+        '''
         for attribute in attributes:
             setattr(self, attribute, int(getattr(self, attribute)))
 
     def _process_socket(self, socket_attr, socket_str, socket_location) -> None:
-        def _translate_addr() -> None:
+        '''
+        Process socket information and set related attributes.
+
+        Args:
+            socket_attr (str): The attribute name to be set.
+            socket_str (str): The socket information string.
+            socket_location (str): The location indicator, either "local" or "remote".
+        '''
+        def _translate_and_set_addr_from_asterisk_to_zeros() -> None:
             setattr(self, f"{socket_location}Addr", f"{'0.0.0.0' if self.family == 4 else '::'}")
         address = '.'.join(socket_str.split('.')[:-1])
         port = socket_str.split('.')[-1]
         if socket_str == '*.*':
             setattr(self, socket_attr, f"{'0.0.0.0:0' if self.family == 4 else ':::0'}")
-            _translate_addr()
+            _translate_and_set_addr_from_asterisk_to_zeros()
             setattr(self, f"{socket_location}Port", 0)
         else:
             if socket_str.startswith('*'):
                 setattr(self, socket_attr, f"{'0.0.0.0' if self.family == 4 else '::'}:{port}")
-                _translate_addr()
+                _translate_and_set_addr_from_asterisk_to_zeros()
             else:
                 setattr(self, socket_attr, f"{address}:{port}")
                 setattr(self, f"{socket_location}Addr", address)
             setattr(self, f"{socket_location}Port", int(port))
 
     def to_csv(self) -> str:
+        '''
+        Return connection attributes as a CSV string.
+
+        Returns:
+            str: CSV-formatted string containing connection attributes.
+        '''
         return (
             f"{self.pid},{self.proto},{self.family},"
             f"{self.localSocket},{self.remoteSocket},"
@@ -67,28 +94,55 @@ class _ConnectionProcessor:
         )
 
     def to_dict(self) -> Dict:
+        '''
+        Return connection attributes as a dictionary.
+
+        Returns:
+            Dict: Dictionary containing connection attributes.
+        '''
         connection_dict = {
             'pid': self.pid,
             'family': self.family, 'proto': self.proto,
             'localAddr': self.localAddr, 'localPort': self.localPort,
-            'remoteAddr': self.remoteAddr, 'remotePort': self.remotePort
+            'remoteAddr': self.remoteAddr, 'remotePort': self.remotePort,
+            'state': self.state, 'state_bits': self.state_bits
         }
-        connection_dict['state'] = self.state
-        connection_dict['state_bits'] = self.state_bits
         return connection_dict
 
     @property
     def as_dict(self) -> Dict:
+        '''
+        Return connection attributes as a dictionary.
+
+        Returns:
+            Dict: Dictionary containing connection attributes.
+        '''
         return self.__dict__
 
     @property
     def hash(self) -> str:
+        '''
+        Return SHA-1 hash of connection attributes.
+
+        Returns:
+            str: SHA-1 hash of connection attributes.
+        '''
         hash_obj = sha1()
         hash_obj.update(str(self.to_dict()).encode('utf-8'))
         return hash_obj.hexdigest()
 
 @dataclass
 class BaseConnection:
+    '''
+    Base class for network connections.
+
+    Attributes:
+        proto (str): Protocol of the connection.
+        recvQ (int): Receive queue size.
+        sendQ (int): Send queue size.
+        localSocket (str): Local socket information.
+        remoteSocket (str): Remote socket information.
+    '''
     proto: str
     recvQ: int
     sendQ: int
@@ -97,14 +151,48 @@ class BaseConnection:
 
 @dataclass
 class TCP_State:
+    '''
+    Class representing TCP connection state.
+
+    Attributes:
+        state (str): TCP connection state.
+    '''
     state: str
 
 @dataclass
 class UDP_State:
+    '''
+    Class representing UDP connection state.
+
+    Attributes:
+        state (str, optional): UDP connection state. Defaults to None.
+    '''
     state: str = None
 
 @dataclass
 class Common_Connection_attrs_and_metrics:
+    '''
+    Common attributes and metrics for network connections.
+
+    Attributes:
+        rhiwat (int): High-water mark for received data.
+        shiwat (int): High-water mark for sent data.
+        pid (int): Process ID associated with the connection.
+        epid (int): Effective process ID associated with the connection.
+        state_bits (str): State bits of the connection.
+        options (str): Connection options.
+        gencnt (str): Generation count.
+        flags (str): Connection flags.
+        flags1 (str): Additional connection flags.
+        usscnt (int): User space send count.
+        rtncnt (int): Return count.
+        fltrs (int): Filters count.
+        family (int, optional): Address family. Defaults to None.
+        localAddr (str, optional): Local address. Defaults to None.
+        localPort (int, optional): Local port. Defaults to None.
+        remoteAddr (str, optional): Remote address. Defaults to None.
+        remotePort (int, optional): Remote port. Defaults to None.
+    '''
     rhiwat: int
     shiwat: int
     pid: int
@@ -128,19 +216,69 @@ class TCP_Connection(
     Common_Connection_attrs_and_metrics,
     TCP_State,
     BaseConnection, _ConnectionProcessor
-): ...
+):
+    '''
+    Class representing a TCP network connection.
+
+    Inherits attributes and methods from Common_Connection_attrs_and_metrics,
+    TCP_State, BaseConnection, and _ConnectionProcessor.
+
+    Example:
+        tcp_connection = TCP_Connection(
+            rhiwat=100, shiwat=200, pid=1234, epid=5678,
+            state_bits='0001', options='-', gencnt='1234',
+            flags='00000002', flags1='-', usscnt=0, rtncnt=0,
+            fltrs=0, proto='tcp', recvQ=0, sendQ=0,
+            localSocket='0.0.0.0:22', remoteSocket='0.0.0.0:0',
+            state='LISTEN'
+        )
+    '''
+    ...
 
 @dataclass
 class UDP_Connection(
     UDP_State,
     Common_Connection_attrs_and_metrics,
     BaseConnection, _ConnectionProcessor
-): ...
+):
+    '''
+    Class representing a UDP network connection.
+
+    Inherits attributes and methods from UDP_State,
+    Common_Connection_attrs_and_metrics, BaseConnection, and _ConnectionProcessor.
+
+    Example:
+        udp_connection = UDP_Connection(
+            rhiwat=100, shiwat=200, pid=1234, epid=5678,
+            state_bits='0001', options='-', gencnt='1234',
+            flags='00000002', flags1='-', usscnt=0, rtncnt=0,
+            fltrs=0, proto='udp', recvQ=0, sendQ=0,
+            localSocket='0.0.0.0:12345', remoteSocket='0.0.0.0:0',
+            state='LISTEN'
+        )
+    '''
+    ...
 
 Net_Connection = Union[TCP_Connection, UDP_Connection]
 
 @dataclass
 class ICMP_Exchange(Common_Connection_attrs_and_metrics,
                      BaseConnection, _ConnectionProcessor):
-    '''To be implemented'''
+    '''
+    Class representing an ICMP network connection.
+
+    To be implemented.
+
+    Inherits attributes and methods from Common_Connection_attrs_and_metrics,
+    BaseConnection, and _ConnectionProcessor.
+
+    Example:
+        icmp_exchange = ICMP_Exchange(
+            rhiwat=100, shiwat=200, pid=1234, epid=5678,
+            state_bits='0001', options='-', gencnt='1234',
+            flags='00000002', flags1='-', usscnt=0, rtncnt=0,
+            fltrs=0, proto='icmp', recvQ=0, sendQ=0,
+            localSocket='0.0.0.0:0', remoteSocket='0.0.0.0:0'
+        )
+    '''
     ...
--- src/Netstat.py
+++ src/Netstat.py
@@ -1,8 +1,8 @@
-import ipaddress
-
 from typing import List, Optional
 from dataclasses import dataclass
 from subprocess import run
+
+from ipaddress import ip_address
 
 from .Common import subprocess_run_kwargs
 from .Connection import Net_Connection, TCP_Connection, UDP_Connection
@@ -12,13 +12,36 @@ families = ('inet', 'inet6')
 
 @dataclass
 class Interface:
+    '''
+    Class representing a network interface.
+
+    Attributes:
+        name (str): The name of the network interface.
+        addresses (List[str], optional): List of IP addresses associated with the interface.
+            Defaults to None.
+    '''
     name: str
     addresses: List[str] = None
 
 class Netstat:
+    '''
+    Class for retrieving network statistics and connections using the `netstat` command.
+
+    Example:
+        netstat_instance = Netstat()
+        interfaces = netstat_instance.get_interfaces()
+        connections = netstat_instance.get_connections(proto='tcp', family='inet')
+
+    '''
 
     @staticmethod
     def get_interfaces() -> List[Interface]:
+        '''
+        Retrieve a list of network interfaces.
+
+        Returns:
+            List[Interface]: List of Interface objects representing network interfaces.
+        '''
         command = "netstat -inl"
         lines = [
             line.split() for line in
@@ -31,16 +54,27 @@ class Netstat:
         interfaces = []
         for iface in set([line[0] for line in lines]):
             iface = Interface(
-                name = iface,
-                addresses = [line[3] for line in lines if line[0] == iface]
+                name=iface,
+                addresses=[line[3] for line in lines if line[0] == iface]
             )
             interfaces.append(iface)
         return interfaces
 
+    @staticmethod
     def _run_netstat_to_get_connections(
             proto: Optional[str] = None,
             family: Optional[str] = None
         ) -> List[str]:
+        '''
+        Run the `netstat` command to retrieve connection information.
+
+        Args:
+            proto (str, optional): Protocol filter (tcp/udp). Defaults to None.
+            family (str, optional): Address family filter (inet/inet6). Defaults to None.
+
+        Returns:
+            List[str]: List of strings containing the output of the `netstat` command.
+        '''
         proto_selector = f'-p {proto}' if proto is not None else ''
         family_selector = f'-f {family}' if family is not None else ''
         netstat_command = f"netstat -nval {family_selector} {proto_selector}"
@@ -48,9 +82,19 @@ class Netstat:
         netstat_lines = netstat_out.splitlines()
         return netstat_lines
 
+    @staticmethod
     def _parse_netstat_connection(
             netstat_connection_line: str
         ) -> Net_Connection:
+        '''
+        Parse a line of `netstat` output and create a Net_Connection object.
+
+        Args:
+            netstat_connection_line (str): A line of `netstat` output.
+
+        Returns:
+            Net_Connection: A Net_Connection object representing the parsed connection.
+        '''
         connection_classes = {'tcp': TCP_Connection, 'udp': UDP_Connection}
         proto = netstat_connection_line.split()[0].lower()
         proto = next(
@@ -67,6 +111,18 @@ class Netstat:
             family: Optional[str] = None,
             netstat_lines: Optional[List[str]] = None
         ) -> List[Net_Connection]:
+        '''
+        Get a list of network connections.
+
+        Args:
+            proto (str, optional): Protocol filter (tcp/udp). Defaults to None.
+            family (str, optional): Address family filter (inet/inet6). Defaults to None.
+            netstat_lines (List[str], optional): Predefined list of `netstat` output lines.
+                Defaults to None.
+
+        Returns:
+            List[Net_Connection]: List of Net_Connection objects representing network connections.
+        '''
         if netstat_lines is None:
             netstat_lines = Netstat._run_netstat_to_get_connections(proto, family)
         connections = [
@@ -79,6 +135,16 @@ class Netstat:
     def get_pids_of_processes_with_connections(
         connections: Optional[List[Net_Connection]] = None
     ) -> List[int]:
+        '''
+        Get a list of process IDs with active network connections.
+
+        Args:
+            connections (List[Net_Connection], optional): List of Net_Connection objects.
+                Defaults to None.
+
+        Returns:
+            List[int]: List of process IDs with active network connections.
+        '''
         if connections is None:
             connections = Netstat.get_connections()
         pids = sorted(set(connection.pid for connection in connections))
@@ -89,10 +155,21 @@ class Netstat:
         interface: str,
         connections: Optional[List[Net_Connection]] = None
     ) -> List[Net_Connection]:
+        '''
+        Get a list of network connections filtered by a specific network interface.
+
+        Args:
+            interface (str): The name or IP address of the network interface.
+            connections (List[Net_Connection], optional): List of Net_Connection objects.
+                Defaults to None.
+
+        Returns:
+            List[Net_Connection]: List of Net_Connection objects filtered by the specified interface.
+        '''
         if connections is None:
             connections = Netstat.get_connections()
         try:
-            ipaddress.ip_address(interface)
+            ip_address(interface)
             connections_by_interface = [
                 connection for connection in connections
                 if connection.localAddr == interface
