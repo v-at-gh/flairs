022bb83b7513ec326a3a91969c757a2d7ea34e6c
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-08 13:27:24 +0300

    improve Filter class

--- src/Filter.py
+++ src/Filter.py
@@ -17,22 +17,35 @@ class Filter:
 
     @staticmethod
     def construct_endpoint_filter(
-            purpose, proto=None, addr=None, port=None
+            purpose, proto=None, addr=None, port=None, filter_goal='exclude'
     ) -> str:
         if purpose not in purposes:
             raise ValueError(f"Invalid purpose: {purpose}")
 
+        inclusion_prefix = 'not ' if filter_goal == 'exclude' else ''
+
         if purpose == 'capture':
-            expression = f'''not ({" or ".join(
-                [f"({direction} host {addr} and {proto} {direction} port {port})"
-                 for direction in directions]
-            )})'''
+            expression = f'''{inclusion_prefix}({" or ".join(
+                    [f"""({direction} {'host'
+                        if len(addr.split('/')) == 1
+                        else 'net'
+                        } {addr} and {proto} {direction} port {port})"""
+                     for direction in directions]
+                )})'''
         elif purpose == 'preview':
-            expression = f'''not ({" or ".join(
+            expression = f'''{inclusion_prefix}({" or ".join(
                 [f"(ip.{direction} == {addr} and {proto}.{direction}port == {port})"
                  for direction in directions]
-            )})'''
+                )})'''
         else:
             expression = ''
 
         return expression
+
+    @staticmethod
+    def tcpdump_filter(**kwargs) -> str:
+        return Filter.construct_endpoint_filter(purpose='capture', **kwargs)
+
+    @staticmethod
+    def wireshark_filter(**kwargs) -> str:
+        return Filter.construct_endpoint_filter(purpose='preview', **kwargs)
