bf0a89c7e92526219c6efdcbc5c90b2d4662e970
vitpodsokhin <vitpodsokhin@gmail.com>
2024-01-08 23:42:15 +0300

    test argument parsing

--- static/filter.js
+++ static/filter.js
@@ -1,10 +1,9 @@
-// Filter.js
 const purposes = ['capture', 'preview'];
 const directions = ['src', 'dst'];
 const filterGoals = ['exclude', 'include'];
 
 class Filter {
-    static constructEndpointFilter(purpose, proto = null, addr = null, port = null, filterGoal = 'exclude') {
+    static constructEndpointFilter(purpose, proto, addr, port, filterGoal = 'exclude') {
         // TODO: maybe we can come up with better naming?
         if (!purposes.includes(purpose)) {
             throw new Error(`Invalid purpose: ${purpose}\nChoose one of ${purposes.join(', ')}`);
@@ -19,11 +18,18 @@ class Filter {
         let packetEnvelope;
 
         if (purpose === 'capture') {
-            packetEnvelope = directions.map(direction => `(${direction} ${
-                addr && addr.split('/').length === 1 ? 'host' : 'net'
-            } ${addr} and ${proto} ${direction} port ${port})`);
+            packetEnvelope = directions.map(direction => {
+                const hostOrNet = addr ? (addr.split('/').length === 1 ? 'host' : 'net') : 'any';
+                const addrPart = addr ? `${hostOrNet} ${addr}` : 'any';
+                const portPart = port ? `and ${proto} ${direction} port ${port}` : '';
+                return `(${direction} ${addrPart} ${portPart})`;
+            });
         } else if (purpose === 'preview') {
-            packetEnvelope = directions.map(direction => `(ip.${direction} == ${addr} and ${proto}.${direction}port == ${port})`);
+            packetEnvelope = directions.map(direction => {
+                const addrPart = addr ? `ip.${direction} == ${addr}` : 'ip.any == any';
+                const portPart = port ? `and ${proto}.${direction}port == ${port}` : '';
+                return `(${addrPart} ${portPart})`;
+            });
         }
 
         const expression = `${inclusionPrefix}(${packetEnvelope.join(' or ')})`;
@@ -63,3 +69,12 @@ const wiresharkFilter = Filter.wiresharkEndpointFilter({
     filterGoal: 'include'
 });
 console.log(wiresharkFilter);
+
+const commonTestFilter = Filter.constructEndpointFilter({
+    purpose: 'preview',
+    proto: 'udp',
+    addr: '10.255.255.0/24',
+    port: 8080,
+    filterGoal: 'exclude'
+});
+console.log(commonTestFilter);
