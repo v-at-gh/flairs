35e3c334536eab3b969bf177816dd1c1dd56114f
vitpodsokhin <vitpodsokhin@gmail.com>
2024-07-17 19:04:32 +0300

    cleanup exclude-address script and it's exit codes

--- exe/exclude-addresses.py
+++ exe/exclude-addresses.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+'''
+    # This script solves the following problem:
+    # https://stackoverflow.com/questions/66204457/how-can-i-remove-two-or-more-subnet-from-a-network
+    # https://codereview.stackexchange.com/questions/245922/ip-address-exclusion-algorithm
+    Since the last answer is technically correct:
+    """
+        Use sets!
+        Lists are O(N), while sets are O(1)!
+        Secondly it's quite easy to subtract sets simply use set(c) = set(a) - set(b)
+    """
+    try to execute:
+    set(ipaddress.ip_network('0.0.0.0/8')) - set(ipaddress.ip_network('1.1.1.1')) - set(ipaddress.ip_network('1.3.1.2'))
+    and see how long it takes. (:
+'''
 
 from argparse import ArgumentParser, Namespace
 
@@ -7,7 +21,7 @@ def parse_arguments() -> Namespace:
     network_help = "Network from which we are excluding addresses"
     addresses_help = "comma or whitespace separated addresses of hosts and/or networks to be excluded"
     separator_help = "separator for a list of resulting networks. Default is the new line"
-    ignore_help = "ignore non-valid input arguments"
+    ignore_help = "ignore non-valid input arguments (except the target network)"
 
     parser = ArgumentParser()
     parser.add_argument('network', type=str, help=network_help)
@@ -24,10 +38,6 @@ def main() -> None:
     from pathlib import Path
     sys_path.append(str(Path(__file__).resolve().parents[1]))
 
-    # This script solves the following problem:
-    # https://codereview.stackexchange.com/questions/245922/ip-address-exclusion-algorithm
-    # https://stackoverflow.com/questions/66204457/how-can-i-remove-two-or-more-subnet-from-a-network
-
     import sys
     from src.net_tools import exclude_addresses, is_string_a_valid_ip_network
 
@@ -38,7 +48,7 @@ def main() -> None:
 
     #TODO 1: Maybe implement the following as a new argument validation function?
     if not is_string_a_valid_ip_network(args.network):
-        print(f"{args.network} is not a valid ip network 1.", file=sys.stderr)
+        print(f"{args.network} is not a valid ip network.", file=sys.stderr)
         sys.exit(1)
     else:
         from ipaddress import ip_network
@@ -65,7 +75,7 @@ def main() -> None:
         else:
             if not ' ' in addresses_str:
                 print(f"{addresses_str} is not a valid ip network.", file=sys.stderr)
-                sys.exit(4)
+                sys.exit(2)
             addresses = set(a.strip() for a in addresses_str.split() if a.strip() != '')
 
         for a in addresses:
@@ -80,14 +90,16 @@ def main() -> None:
         print(separator.join((str(n) for n in resulting_networks)).strip(), file=sys.stdout)
 
     if not args.ignore and (invalid_addresses or misfitting_addresses or irrelevant_addresses):
-        invalid_addresses_str = \
-            f"{'invalid_addresses_str: '+' '.join(str(item) for item in invalid_addresses) if len(invalid_addresses)>0 else ''}"
-        misfitting_addresses_str = \
-            f"{'misfitting_addresses_str: '+' '.join(str(item) for item in misfitting_addresses) if len(misfitting_addresses)>0 else ''}"
-        irrelevant_addresses_str = \
-            f"{'irrelevant_addresses_str: '+' '.join(str(item) for item in irrelevant_addresses) if len(irrelevant_addresses)>0 else ''}"
-        print('\n'.join([invalid_addresses_str, misfitting_addresses_str, irrelevant_addresses_str]).strip(), file=sys.stderr)
-        sys.exit(1)
+        wrong_stuff_message_list = []
+        for wrong_stuff in zip(
+                ('invalid addresses', 'misfitting addresses', 'irrelevant addresses'),
+                (invalid_addresses, misfitting_addresses, irrelevant_addresses)):
+            if len(wrong_stuff[1]) > 0:
+                wrong_stuff_message_list.append(
+                    f"{wrong_stuff[0]+': '+' '.join(str(item) for item in wrong_stuff[1])}"
+                )
+        print('\n'.join(wrong_stuff_message_list).strip(), file=sys.stderr)
+        sys.exit(2)
     else:
         resulting_networks = sorted(list(exclude_addresses(target_network, (a for a in address_objs))))
         print_result(resulting_networks)
